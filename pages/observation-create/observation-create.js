c9a8d6f2bdfbb3b38927f3e5f318839318fd9285
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2023-12-16 23:12:44 +0800

    feat: 图片上传

--- pages/observation-create/observation-create.js
+++ pages/observation-create/observation-create.js
@@ -3,10 +3,6 @@ var UUID = require("pure-uuid")
 
 // pages/observation-create/observation-create.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     fileList: [],
   },
@@ -27,9 +23,7 @@ Page({
       filePath: file.url,
       key: uuid,
       success: (res) => {
-        debugger
         const index = this.data.fileList.findIndex(i => i.key === uuid)
-        debugger
         this.setData({
           [`fileList[${index}].status`]: 'done',
         });
@@ -79,59 +73,13 @@ Page({
       }
     })
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+  handleRemove(e) {
+    const { index } = e.detail;
+    const { fileList } = this.data;
 
+    fileList.splice(index, 1);
+    this.setData({
+      fileList,
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
