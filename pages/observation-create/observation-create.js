5bdac0342ba4f0f9d0fdbea793e8d7a21c471065
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2023-12-17 17:00:38 +0800

    feat: 解析经纬度

--- pages/observation-create/observation-create.js
+++ pages/observation-create/observation-create.js
@@ -1,19 +1,41 @@
 const uploadOSS = require("../../utils/uploadOSS");
 const {getOSSUrlByKey} = require("../../utils/util")
 const UUID = require("pure-uuid")
-var myexif = require('../../utils/exif');
+var EXIF = require('../../utils/exif');
+const computedBehavior = require('miniprogram-computed').behavior
 
 // pages/observation-create/observation-create.js
 Page({
+  behaviors: [computedBehavior],
   data: {
     fileList: [],
     note: '',
     time: '',
     timeSelectorVisible: false,
-    address: '',
+    address: null,
     taxon: '',
     addressVisible: '',
     artificial: false,
+  },
+  computed: {
+    formattedLatitude(data) {
+      // 注意： computed 函数中不能访问 this ，只有 data 对象可供访问
+      // 这个函数的返回值会被设置到 this.data.sum 字段中
+      if(data.address?.GPSLatitude) {
+        return `${data.address?.GPSLatitude[0]}°${data.address?.GPSLatitude[1]}'${data.address?.GPSLatitude[2]}"${data.address?.GPSLatitudeRef}`
+      }
+    },
+    formattedLongitude(data) {
+      if(data.address?.GPSLongitude) {
+        return `${data.address?.GPSLongitude[0]}°${data.address?.GPSLongitude[1]}'${data.address?.GPSLongitude[2]}"${data.address?.GPSLongitudeRef}`
+      }
+    },
+    formattedAltitude(data) {
+      if(data.address?.GPSAltitude) {
+        const prefix = data.address.GPSAltitudeRef === 0 ? '' : '-'
+        return `${prefix}${Math.floor(data.address?.GPSAltitude)}米`
+      }
+    }
   },
   noteChange(e) {
     this.setData({note: e.detail.value})
@@ -29,21 +51,31 @@ Page({
       src: file.url,
     }).then((res) => {
       file.metaData = res
-      var array = wx.getFileSystemManager().readFileSync(file.url);;
+      const array = wx.getFileSystemManager().readFileSync(file.url);
 
-      var res = myexif.handleBinaryFile(array);
-      const GPSLatitude = res?.data?.GPSLatitude
-      const GPSLongitude = res?.data?.GPSLongitude
-      wx.showModal({
-        title: '',
-        content: JSON.stringify(res),
-      })
-      if(GPSLatitude) {
-        file.metaData.GPSLatitude = GPSLatitude
-        file.metaData.GPSLongitude = GPSLongitude
-        this.setData({address: [GPSLatitude, GPSLongitude]})
+      var exifInfo = JSON.parse(JSON.stringify(EXIF.handleBinaryFile(array)));
+
+      const time = exifInfo?.data?.DateTime
+      if(time && !this.time) {
+        this.setData({
+          time: time
+       })
+       file.metaData.time = time
       }
 
+      if(exifInfo?.data?.GPSLatitude && !this.address) {
+        const GPSInfo = {
+          // 参考文档 https://exiftool.org/TagNames/GPS.html
+           GPSLatitude: exifInfo?.data?.GPSLatitude,
+           GPSLatitudeRef: exifInfo?.data?.GPSLatitudeRef,
+           GPSLongitude: exifInfo?.data?.GPSLongitude,
+           GPSLongitudeRef: exifInfo?.data?.GPSLongitudeRef,
+           GPSAltitude: exifInfo?.data?.GPSAltitude,
+           GPSAltitudeRef: exifInfo?.data?.GPSAltitudeRef,
+        }
+        file.metaData.gpsInfo = GPSInfo
+        this.setData({address: GPSInfo})
+      }
     }).then(() => {
       this.setData({
         fileList: [...fileList, { ...file, key: uuid, status: 'loading' }],
