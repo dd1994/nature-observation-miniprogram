2bce2253f644f184e4fbdf52002fa5dd1caeba27
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2023-12-17 18:52:00 +0800

    feat: 搜索

--- pages/taxon-picker/taxon-picker.js
+++ pages/taxon-picker/taxon-picker.js
@@ -5,9 +5,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-    inputWords: ''
+    inputWords: '',
+    searchResult: []
   },
   searchWords(e) {
     // 请求接口
+    wx.request({
+      url: 'http://www.sp2000.org.cn/api/v2/getNameByKeyword',
+      data: {
+        apiKey: '95cf54522c3c48c88d4ab800ec33eb0f',
+        keyword: this.data.inputWords
+      },
+      success: (res) => {
+        if(res?.data?.names?.length) {
+          this.setData({searchResult: res.data.names})
+        }
+      },
+      fail: (err) => {
+        this.setData({searchResult: []})
+      }
+    })
   }
 })
