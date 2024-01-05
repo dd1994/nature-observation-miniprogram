7e59b08a23285fb7f03eccb6cc643ecc43919295
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-01-05 20:19:26 +0800

    refactor

--- pages/taxon-detail/taxon-detail.js
+++ pages/taxon-detail/taxon-detail.js
@@ -1,66 +1,105 @@
-// pages/taxon-detail/taxon-detail.js
+const computedBehavior = require('miniprogram-computed').behavior
+import { openBirdDetail, openMammalDetail } from '../../utils/openTaxonDetail'
+import { fetchPlantDetail, fetchAnimalDetail } from '../../utils/service'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  behaviors: [computedBehavior],
   data: {
-
+    taxon: {
+      rank: 'species',
+      name: 'Dioscorea polystachya',
+      "iconic_taxon_name": "Plantae",
+    },
+    taxonDetail: null,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  computed: {
+    taxonGroup(data) {
+      return data.taxon?.iconic_taxon_name
+    },
+    useAnimalDB(data) {
+      return ['Actinopterygii', 'Insecta', 'Mollusca', 'Amphibia'].includes(data.taxon?.iconic_taxon_name)
+    },
+    useEmbeddedMiniProgram(data) {
+      return ['Aves', 'Mammalia'].includes(data.taxon?.iconic_taxon_name)
+    },
   },
+  openDetail() {
+    const taxon = this.data.taxon
+    const defaultAction = () => {
+      wx.showToast({
+        title: '暂时没有更多关于该物种的信息了~',
+        icon: 'none',
+        duration: 2500
+      })
+    }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+    const defaultAnimalAction = () => {
+      fetchAnimalDetail({
+        name: taxon.name
+      }).then(res => {
+        if (res?.length) {
+          this.setData({
+            taxonDetail: {
+              detail: res,
+              ...taxon
+            }
+          })
+        } else {
+          this.setData({
+            taxonDetail: taxon
+          })
+        }
+      }).catch(err => {
+        this.setData({
+          taxonDetail: taxon
+        })
+      })
+    }
 
+    const actionMap = {
+      "Aves": () => {
+        if (taxon.rank === "species") {
+          openBirdDetail(taxon.name)
+        }
+      },
+      "Plantae": () => {
+        fetchPlantDetail({
+          name: taxon.name,
+          success: (res) => {
+            debugger
+            if (res?.data?.frpsdesc) {
+              this.setData({
+                taxonDetail: {
+                  ...res.data,
+                  ...taxon
+                }
+              })
+            } else {
+              this.setData({
+                taxonDetail: taxon
+              })
+            }
+          },
+          fail: (err) => {
+            this.setData({
+              taxonDetail: taxon
+            })
+          }
+        })
+      },
+      'Actinopterygii': defaultAnimalAction,
+      'Insecta': defaultAnimalAction,
+      'Mollusca': defaultAnimalAction,
+      'Amphibia': defaultAnimalAction,
+      "Mammalia": () => {
+        if (taxon.rank === "species") {
+          openMammalDetail(taxon.name)
+        }
+      }
+    }
+      ; (actionMap[taxon.iconic_taxon_name] || defaultAction)()
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  onLoad() {
+    this.openDetail()
   }
 })
