12f7d682a83033d62c83634370101c5d93570818
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-01-19 19:08:32 +0800

    fix

--- components/observation-list/observation-list.ts
+++ components/observation-list/observation-list.ts
@@ -1,26 +1,13 @@
-// components/observation-list/observation-list.js
 Component({
-
-  /**
-   * 组件的属性列表
-   */
   properties: {
     observations: {
       type: Array,
       value: []
     },
   },
-
-  /**
-   * 组件的初始数据
-   */
   data: {
 
   },
-
-  /**
-   * 组件的方法列表
-   */
   methods: {
     gotoDetailPage(e) {
       wx.navigateTo({
