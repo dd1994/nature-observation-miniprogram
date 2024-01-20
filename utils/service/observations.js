c8864a33f4b65d74e5b35024599162fe71241a12
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-01-20 08:55:40 +0800

    feat: 统一使用 post 请求

--- utils/service/observations.js
+++ utils/service/observations.js
@@ -2,13 +2,14 @@ import { requestPromise, requestPromiseWithLogin } from "../util"
 
 export function fetchObservationList() {
   return requestPromiseWithLogin({
-    url: 'http://192.168.3.40:7001/api/v1/observations'
+    url: 'http://192.168.3.40:7001/api/v1/observations',
+    method: 'POST',
   })
 }
 
 export function createObservation(params) {
   return requestPromiseWithLogin({
-    url: 'http://192.168.3.40:7001/api/v1/observations',
+    url: 'http://192.168.3.40:7001/api/v1/observations/create',
     method: 'POST',
     data: params
   })
@@ -22,15 +23,15 @@ export function fetchObservationDetail(id) {
 
 export function updateObservation(params, id) {
   return requestPromiseWithLogin({
-    url: 'http://192.168.3.40:7001/api/v1/observations/' + id,
-    method: 'put',
+    url: 'http://192.168.3.40:7001/api/v1/observations/update/' + id,
+    method: 'post',
     data: params
   })
 }
 
 export function deleteObservation(id) {
   return requestPromiseWithLogin({
-    url: 'http://192.168.3.40:7001/api/v1/observations/' + id,
-    method: 'DELETE',
+    url: 'http://192.168.3.40:7001/api/v1/observations/delete/' + id,
+    method: 'post',
   })
 }
