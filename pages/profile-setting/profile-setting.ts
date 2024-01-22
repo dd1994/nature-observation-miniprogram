2e43a9c9f781476613b15c01f6ad63a60f6e4f61
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-01-22 20:36:48 +0800

    feat: 个人资料设置页

--- pages/profile-setting/profile-setting.ts
+++ pages/profile-setting/profile-setting.ts
@@ -1,66 +1,18 @@
+import { getUserProfile } from "../../utils/service/user"
+
 // pages/profile-setting/profile-setting.ts
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    userProfile: null
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  getUserProfile() {
+    return getUserProfile().then(res => {
+      this.setData({
+        userProfile: res?.data?.data
+      })
+    })
+  },
   onLoad() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+    this.getUserProfile()
   }
 })
