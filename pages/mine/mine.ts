611e34ccb08f270a289a0a7df2012a7c2cf22da9
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-01-22 21:39:36 +0800

    refactor

--- pages/mine/mine.ts
+++ pages/mine/mine.ts
@@ -3,43 +3,16 @@ import { getUserProfile } from "../../utils/service/user"
 import { isLogin } from "../../utils/util";
 const computedBehavior = require('miniprogram-computed').behavior;
 
+const app = getApp()
 Page({
   behaviors: [computedBehavior],
   data: {
-    user: null,
+    user: app.globalData.userInfo,
     loginLoding: false
   },
   computed: {
     displayName(data) {
       return data.user?.user_name || '点击登录'
     }
-  },
-  login() {
-    if (isLogin()) {
-      return
-    }
-    wx.showLoading({
-      title: '登录中...'
-    })
-    this.setData({
-      loginLoding: true
-    })
-    login().then(() => {
-      return this.getUserProfile()
-    }).finally(() => {
-      wx.showToast({
-        title: '登录成功'
-      })
-    })
-  },
-  getUserProfile() {
-    return getUserProfile().then(res => {
-      this.setData({
-        user: res?.data?.data
-      })
-    })
-  },
-  onTabItemTap() {
-    this.getUserProfile()
   }
 })
