a51fd21c4b255dacac86b0bbe5bdfc4c307a51c5
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-01-22 12:12:00 +0800

    feat: 登录操作

--- pages/mine/mine.ts
+++ pages/mine/mine.ts
@@ -1,14 +1,41 @@
+import { login } from "../../utils/service/login";
 import { getUserProfile } from "../../utils/service/user"
+const computedBehavior = require('miniprogram-computed').behavior;
 
 Page({
+  behaviors: [computedBehavior],
   data: {
-    user: null
+    user: null,
+    loginLoding: false
   },
-  onTabItemTap() {
-    getUserProfile().then(res => {
+  computed: {
+    displayName(data) {
+      return data.user?.user_name || '点击登录'
+    }
+  },
+  login() {
+    wx.showLoading({
+      title: '登录中...'
+    })
+    this.setData({
+      loginLoding: true
+    })
+    login().then(() => {
+      return this.getUserProfile()
+    }).finally(() => {
+      wx.showToast({
+        title: '登录成功'
+      })
+    })
+  },
+  getUserProfile() {
+    return getUserProfile().then(res => {
       this.setData({
         user: res?.data?.data
       })
     })
+  },
+  onTabItemTap() {
+    this.getUserProfile()
   }
 })
