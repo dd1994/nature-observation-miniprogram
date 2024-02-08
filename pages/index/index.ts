fa036e81fd8bb012f6ffd9bae19d51ad80a3a405
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-02-08 19:30:35 +0800

    feat: 分页查询

--- pages/index/index.ts
+++ pages/index/index.ts
@@ -8,16 +8,16 @@ Page({
   behaviors: [computedBehavior, UserProfileBehavior],
   data: {
     observations: [],
-    pageSize: 20,
+    pageSize: 21,
     pageIndex: 1,
     total: 0,
     isEmpty: false,
   },
   fetchObservationList() {
-    return fetchObservationList({ pageIndex: this.data.pageIndex, pageSize: this.data.pageSize }).then(res => {
+    return fetchObservationList({ pageIndex: this.data.pageIndex, pageSize: this.data.pageSize }).then((res: any) => {
       this.setData({
-        // @ts-ignore
-        observations: (this.data.observations || []).concat(res?.data?.data || [])
+        observations: (this.data.observations || []).concat(res?.data?.data?.list || []),
+        total: res?.data?.data?.total_count || 0
       })
       if (!this.data.observations.length && (this.pageIndex === 1)) {
         this.setData({
