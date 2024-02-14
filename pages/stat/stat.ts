8156660fe96549a660578191cb9e69426687622e
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-02-14 10:57:47 +0800

    fix

--- pages/stat/stat.ts
+++ pages/stat/stat.ts
@@ -1,11 +1,57 @@
 // pages/stat/stat.ts
+import * as echarts from '../../components/ec-canvas/echarts';
+
+function initChart(canvas, width, height, dpr) {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height,
+    devicePixelRatio: dpr // new
+  });
+  canvas.setChart(chart);
+
+  var option = {
+    backgroundColor: "#ffffff",
+    series: [{
+      label: {
+        normal: {
+          fontSize: 14
+        }
+      },
+      type: 'pie',
+      center: ['50%', '50%'],
+      radius: ['20%', '40%'],
+      data: [{
+        value: 55,
+        name: '北京'
+      }, {
+        value: 20,
+        name: '武汉'
+      }, {
+        value: 10,
+        name: '杭州'
+      }, {
+        value: 20,
+        name: '广州'
+      }, {
+        value: 38,
+        name: '上海'
+      }]
+    }]
+  };
+
+  chart.setOption(option);
+  return chart;
+}
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    ec: {
+      onInit: initChart
+    }
   },
 
   /**
