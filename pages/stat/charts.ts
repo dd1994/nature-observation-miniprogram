d921d6ad31a95d2124ffc89388d182ef2ee8e03b
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-02-14 15:50:06 +0800

    fix

--- pages/stat/charts.ts
+++ pages/stat/charts.ts
@@ -70,49 +70,7 @@ export function initTaxonMap(canvas, width, height, dpr) {
       {
         type: 'map',
         mapType: 'china',
-
-        label: {
-          normal: {
-          },
-          emphasis: {
-            textStyle: {
-              color: '#fff',
-            },
-          },
-        },
-
-        itemStyle: {
-          normal: {
-            borderColor: '#389BB7',
-            // areaColor: '#fff',
-          },
-          emphasis: {
-            areaColor: '#389BB7',
-            borderWidth: 0,
-          },
-        },
-
-        data: [
-          { name: '郑州市', value: 100 },
-          { name: '洛阳市', value: 10 },
-          { name: '开封市', value: 20 },
-          { name: '信阳市', value: 30 },
-          { name: '驻马店市', value: 40 },
-          { name: '南阳市', value: 41 },
-          { name: '周口市', value: 15 },
-          { name: '许昌市', value: 25 },
-          { name: '平顶山市', value: 35 },
-          { name: '新乡市', value: 35 },
-          { name: '漯河市', value: 35 },
-          { name: '商丘市', value: 35 },
-          { name: '三门峡市', value: 35 },
-          { name: '济源市', value: 35 },
-          { name: '焦作市', value: 35 },
-          { name: '安阳市', value: 35 },
-          { name: '鹤壁市', value: 35 },
-          { name: '濮阳市', value: 35 },
-          { name: '开封市', value: 45 },
-        ],
+        data: [],
       },
     ],
   };
