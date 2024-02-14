b87eb7207a3a73bd6727468dc16d57625fd86c88
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-02-14 20:37:35 +0800

    fix

--- pages/stat/charts.ts
+++ pages/stat/charts.ts
@@ -287,7 +287,7 @@ export function initWordCloudChart(canvas, width, height, dpr) {
         rotationRange: [0, 0],
         //渲染的梯度就是 rotationStep ，这个值越小，词云里出现的角度种类就越多
         rotationStep: 0,
-        shape: "circle",
+        shape: "square",
         textStyle: {
           color: function () {
             return "#FFF"
