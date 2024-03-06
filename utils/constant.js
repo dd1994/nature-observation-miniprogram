36d15c8b803db7ce6d057c9861f82b107fdede3e
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-03-07 00:24:17 +0800

    fix

--- utils/constant.js
+++ utils/constant.js
@@ -1,5 +1,5 @@
 export const defaultTimeFormat = 'YYYY-MM-DD'
 export const exifTimeFormat = 'YYYY:MM:DD HH:mm:ss'
-// export const apiDomain = 'http://123.57.13.211:7001'
-export const apiDomain = 'http://192.168.3.179:7001'
+export const apiDomain = 'http://123.57.13.211:7001'
+// export const apiDomain = 'http://192.168.3.179:7001'
 export const statBgColor = '#0bae31'
