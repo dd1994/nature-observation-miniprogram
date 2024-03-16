1f256bd9574b60d5033c9ee74e360fb54f6052ed
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-03-16 21:10:03 +0800

    fix

--- mine-packages/pages/ai-tools/ai-tools.ts
+++ mine-packages/pages/ai-tools/ai-tools.ts
@@ -14,5 +14,17 @@ Page({
   },
   openXiaochong() {
     this.openByAppId('wxa0283c99163682c1')
+  },
+  openXingse() {
+    this.openByAppId('wx66aed246411799b2')
+  },
+  openHuabanlv() {
+    this.openByAppId('wx28184e60cbbdc44b')
+  },
+  openWenhao() {
+    this.openByAppId('wx2f147e41f13ba243')
+  },
+  openDongniao() {
+    this.openByAppId('wx0d6a77cb8b27cf91')
   }
 })
