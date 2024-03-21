b828d827a24ed8e6e261e5e72f851f0d22dd4435
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-03-21 21:34:33 +0800

    fix

--- mine-packages/pages/user-home/user-home.ts
+++ mine-packages/pages/user-home/user-home.ts
@@ -1,11 +1,23 @@
+import { getUserProfileWithOutLogin } from "../../../utils/service/user"
+
 Page({
   data: {
     user_id: null,
+    userProfile: null,
   },
 
   onLoad(options) {
     this.setData({
       user_id: options.user_id
+    })
+    this.getUserProfile()
+  },
+  getUserProfile() {
+    getUserProfileWithOutLogin(this.data.user_id).then(res => {
+      this.setData({
+        // @ts-ignore
+        userProfile: res?.data?.data
+      })
     })
   }
 })
