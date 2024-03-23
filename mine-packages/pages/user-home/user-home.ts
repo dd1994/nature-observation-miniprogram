44219b88698cd6a8d3808dfe90e4bf4b4e5d65cc
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-03-23 20:42:04 +0800

    fix

--- mine-packages/pages/user-home/user-home.ts
+++ mine-packages/pages/user-home/user-home.ts
@@ -1,16 +1,53 @@
+import ObservationsBehavior from "../../../components/observation-list/observationBehavior"
+import TaxonBehavior from "../../../components/taxon-list/taxonBehavior"
+import { TabType } from "../../../pages/index/constant";
 import { getUserProfileWithOutLogin } from "../../../utils/service/user"
+const computedBehavior = require('miniprogram-computed').behavior;
 
 Page({
+  behaviors: [computedBehavior, ObservationsBehavior, TaxonBehavior],
   data: {
+    activeTab: TabType.observations,
     user_id: null,
     userProfile: null,
   },
-
+  computed: {
+    tab1Title(data) {
+      return `记录(${data.observationsTotal})`
+    },
+    tab2Title(data) {
+      return `物种(${data.taxonTotal})`
+    },
+  },
   onLoad(options) {
     this.setData({
       user_id: options.user_id
     })
     this.getUserProfile()
+    this.resetAndFetchObservations()
+    this.resetAndFetchTaxon()
+  },
+  onReachBottom() {
+    if (this.data.activeTab === TabType.observations) {
+      if (this.data.observationsAllLoaded) {
+        return
+      }
+      this.setData({
+        observationsPageIndex: this.data.observationsPageIndex + 1
+      })
+      this.fetchObservationList()
+    } else {
+      if (this.data.taxonAllLoaded) {
+        return
+      }
+      this.setData({
+        taxonPageIndex: this.data.taxonPageIndex + 1
+      })
+      this.fetchTaxonList()
+    }
+  },
+  activeTabChange(e) {
+    this.setData({ activeTab: e.detail.value })
   },
   getUserProfile() {
     getUserProfileWithOutLogin(this.data.user_id).then(res => {
