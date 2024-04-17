f7426989ba3fe526bb54f10fa8f75435aedad49e
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-04-17 20:23:41 +0800

    feat: 支持分享

--- mine-packages/pages/stat/stat.ts
+++ mine-packages/pages/stat/stat.ts
@@ -121,5 +121,9 @@ Page({
       path: `mine-packages/pages/stat/stat?user_id=${this.data.user_id}`
     }
   },
-  
+  onShareTimeline() {
+    return {
+      title: `${this.data?.userProfile?.user_name}的自然观察统计`,
+    }
+  }
 })
