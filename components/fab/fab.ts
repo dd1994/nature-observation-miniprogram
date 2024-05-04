dfa8ae57bcd44f080f64b93ed32a8a0a42a6b0cd
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-05-04 20:23:15 +0800

    fix

---
+++ components/fab/fab.ts
@@ -0,0 +1,26 @@
+// components/fab/fab.ts
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    icon: {
+      type: String
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
