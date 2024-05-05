bb49dac9271cd91f605b81c3077d8aa5d532ccfa
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-05-05 10:02:19 +0800

    fix

--- mine-packages/pages/notify/notify.ts
+++ mine-packages/pages/notify/notify.ts
@@ -12,6 +12,7 @@ Page({
         rightImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
         id: '239882383289',
         title: '小蜘蛛建议了物种鉴定:熊蜂属',
+        desc: "这是一条描述金海环境好久好久合计合计好家伙好家伙交换机家伙好家伙交换机家伙好家伙交换机家伙好家伙交换机家伙好家伙交换机",
         time: '2023-10-10',
         url: '/pages/observation-detail/observation-detail?id=7192684198300225536'
       },
@@ -19,7 +20,7 @@ Page({
         leftImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
         rightImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
         id: '2398823383289',
-        title: '小蜘蛛建议了物种鉴定:熊蜂属',
+        title: '小蜘蛛建议了物种鉴定:熊蜂属熊蜂属熊蜂属熊蜂属熊蜂属熊蜂属熊蜂属熊蜂属熊蜂属熊蜂属熊蜂属熊蜂属',
         desc: '这是一条描述',
         time: '2023-10-10',
         url: '/pages/observation-detail/observation-detail?id=7192684198300225536'
