483ac995eb10c197504b1281e58810dba7fc7a32
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-05-05 09:59:17 +0800

    feat: 通知列表

--- mine-packages/pages/notify/notify.ts
+++ mine-packages/pages/notify/notify.ts
@@ -1,11 +1,54 @@
-// mine-packages/pages/notify/notify.ts
-Page({
+import moment from 'moment'
+moment.locale('zh-cn')
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    notifyList: [
+      {
+        leftImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
+        rightImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
+        id: '239882383289',
+        title: '小蜘蛛建议了物种鉴定:熊蜂属',
+        time: '2023-10-10',
+        url: '/pages/observation-detail/observation-detail?id=7192684198300225536'
+      },
+      {
+        leftImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
+        rightImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
+        id: '2398823383289',
+        title: '小蜘蛛建议了物种鉴定:熊蜂属',
+        desc: '这是一条描述',
+        time: '2023-10-10',
+        url: '/pages/observation-detail/observation-detail?id=7192684198300225536'
+      },
+      {
+        leftImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
+        rightImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
+        id: '2398823583289',
+        title: '小蜘蛛建议了物种鉴定:熊蜂属',
+        time: '2023-10-10',
+        url: '/pages/observation-detail/observation-detail?id=7192684198300225536'
+      },
+      {
+        leftImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
+        rightImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
+        id: '23988232383289',
+        title: '小蜘蛛建议了物种鉴定:熊蜂属',
+        time: '2023-10-10',
+        url: '/pages/observation-detail/observation-detail?id=7192684198300225536'
+      },
+      {
+        leftImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
+        rightImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
+        id: '23988232383289',
+        title: '小蜘蛛建议了物种鉴定:熊蜂属',
+        time: '2023-10-10',
+        url: '/pages/observation-detail/observation-detail?id=7192684198300225536'
+      },
+    ],
   },
 
 })
