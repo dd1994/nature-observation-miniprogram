4e35175bf061fae1ac0f723f2f2cca05c237d674
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-05-05 10:06:26 +0800

    fix

--- mine-packages/pages/notify/notify.ts
+++ mine-packages/pages/notify/notify.ts
@@ -26,10 +26,11 @@ Page({
         url: '/pages/observation-detail/observation-detail?id=7192684198300225536'
       },
       {
-        leftImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
+
         rightImage: 'https://nature-observation-user-avatar.oss-cn-beijing.aliyuncs.com/ddd4fbd0-fd83-11ee-82ed-776b10f5ada2',
         id: '2398823583289',
         title: '小蜘蛛建议了物种鉴定:熊蜂属',
+        desc: '赶快来参加吧~',
         time: '2023-10-10',
         url: '/pages/observation-detail/observation-detail?id=7192684198300225536'
       },
