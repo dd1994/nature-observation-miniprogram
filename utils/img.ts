5f8fdd16ee7d39f32d5d27b78f0e153ca0dfcf82
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-05-12 13:33:44 +0800

    webp

--- utils/img.ts
+++ utils/img.ts
@@ -1,3 +1,3 @@
 export const getImgQuality = (url: string, percent) => {
-  return url + `?x-oss-process=image/resize,p_${percent}`
+  return url + `?x-oss-process=image/resize,p_${percent}/format,webp`
 }
