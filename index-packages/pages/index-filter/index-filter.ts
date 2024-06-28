e5524b821f71c89c4ff955e86b9d899c88b3010f
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-06-28 22:14:24 +0800

    fix

--- index-packages/pages/index-filter/index-filter.ts
+++ index-packages/pages/index-filter/index-filter.ts
@@ -6,6 +6,7 @@ Page({
   behaviors: [computedBehavior],
   data: {
     region: ['全部', '全部', '全部'],
+    taxon: null,
   },
   bindRegionChange(e) {
     this.setData({
@@ -15,6 +16,35 @@ Page({
   removeRegion() {
     this.setData({
       region: ['全部', '全部', '全部'],
+    })
+  },
+  gotoTaxonFilter() {
+    wx.navigateTo({
+      url: '/pages/taxon-picker/taxon-picker',
+      success: (res) => {
+        // 发送一个事件
+        res.eventChannel.emit('setPlaceholder', { placeholder: '搜索感兴趣的类群，比如“蜘蛛目”' })
+      },
+      events: {
+        backFromSearchPage: (taxon) => {
+          if (!taxon?.id) {
+            return
+          }
+
+          this.setData({
+            taxon: {
+              taxon_id: taxon?.id,
+              taxon_preferred_common_name: taxon?.preferred_common_name || taxon?.name,
+              taxon_name: taxon?.name,
+            }
+          })
+        }
+      }
+    })
+  },
+  removeTaxonFilter() {
+    this.setData({
+      taxon: null,
     })
   },
   computed: {
