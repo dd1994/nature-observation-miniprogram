02dfdfe2c570286dbb4630ef756417d83fb38330
赵蒙 <zhaomeng@zhaomengdeMacBook-Pro.local>
2024-06-28 22:03:58 +0800

    fix

--- index-packages/pages/index-filter/index-filter.ts
+++ index-packages/pages/index-filter/index-filter.ts
@@ -1,5 +1,25 @@
+import { generateDisplayRegion } from "../../../utils/util";
+
+const computedBehavior = require('miniprogram-computed').behavior;
+
 Page({
+  behaviors: [computedBehavior],
   data: {
-    taxon: '',
+    region: ['全部', '全部', '全部'],
+  },
+  bindRegionChange(e) {
+    this.setData({
+      region: e.detail.value,
+    })
+  },
+  removeRegion() {
+    this.setData({
+      region: ['全部', '全部', '全部'],
+    })
+  },
+  computed: {
+    displayRegion(data) {
+      return generateDisplayRegion(data.region)
+    }
   }
 })
